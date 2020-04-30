de87ef62cbdd1a80b5ed339ef70998d86a2dde92
Coursera Learner <nobody@nowhere.nul>
2020-04-30 18:30:18 +0000

    Did Assignment 06.

--- 06_rect/rectangle.c
+++ 06_rect/rectangle.c
@@ -16,15 +16,44 @@ int max (int a, int b) {
 }
 
 //Declare your rectangle structure here!
+struct rectangle_p{
+  int x,y,width,height;
+};
 
+typedef struct rectangle_p rectangle;
 
 rectangle canonicalize(rectangle r) {
   //WRITE THIS FUNCTION
+  if(r.width<0){
+    r.x = r.x+r.width;
+    r.width = r.width*(-1);
+  }
+  if(r.height<0){
+    r.y = r.y+r.height;
+    r.height = r.height*(-1);
+  }
   return r;
 }
+
+  
 rectangle intersection(rectangle r1, rectangle r2) {
   //WRITE THIS FUNCTION
-  return r1;
+  rectangle r;
+  r1 = canonicalize(r1);
+  r2 = canonicalize(r2);
+  if((r1.x==r2.x) && (r1.y==r1.y) && (r1.width==r2.width) && (r1.height == r2.height))
+    return r1;
+  else if (((r1.y>(r2.y+r2.height)) || (r1.x+r1.width)<r2.x) || ((r1.y+r1.height)<r2.y) || r1.x> (r2.x+r2.width)){
+    r.width=0;
+    r.height=0;
+  }
+  else{
+    r.x = max(r1.x, r2.x);
+    r.y = max(r1.y, r2.y);
+    r.width = min(r2.x+r2.width, r1.x+r1.width) - max(r1.x, r2.x);
+    r.height = min(r2.y+r2.height, r1.y+r1.height) - max(r1.y, r2.y);
+  }
+  return r;
 }
 
 //You should not need to modify any code below this line
@@ -34,8 +63,7 @@ void printRectangle(rectangle r) {
     printf("<empty>\n");
   }
   else {
-    printf("(%d,%d) to (%d,%d)\n", r.x, r.y, 
-	                           r.x + r.width, r.y + r.height);
+    printf("(%d,%d) to (%d,%d)\n", r.x, r.y, r.x + r.width, r.y + r.height);
   }
 }
 
