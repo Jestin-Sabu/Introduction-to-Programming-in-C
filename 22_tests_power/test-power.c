44f1acc5120fc63a95d3a42f68832cfda923db95
Coursera Learner <nobody@nowhere.nul>
2020-05-04 14:17:55 +0000

    Modified to check the new program.

--- 22_tests_power/test-power.c
+++ 22_tests_power/test-power.c
@@ -5,25 +5,46 @@
 unsigned power(unsigned x, unsigned y);
 
 int main(void){
-  if(power(0,0) !=1)
+  if(power(0,0) !=1){
+    printf("Failed.\n");
     return EXIT_FAILURE;
-  if(power(2,0) !=1)
+  }
+  if(power(2,0) !=1){
+    printf("Failed.\n");
     return EXIT_FAILURE;
-  if(power(-2,0) !=1)
+  }
+  if(power(-2,0) !=1){
+    printf("Failed.\n");
     return EXIT_FAILURE;
-  if(power(2,2) !=4)
+  }
+  if(power(2,2) !=4){
+    printf("Failed.\n");
     return EXIT_FAILURE;
-  if(power(0,5) !=0)
+  }
+  if(power(0,5) !=0){
+    printf("Failed.\n");
     return EXIT_FAILURE;
-  if(power(-2,2) !=4)
+  }
+  if(power(-2,2) !=4){
+    printf("Failed.\n");
     return EXIT_FAILURE;
-  if(power(1,10) !=1)
+  }
+  if(power(1,10) !=1){
+    printf("Failed.\n");
     return EXIT_FAILURE;
-  if(power(3,2) !=9)
+  }
+  if(power(3,2) !=9){
+    printf("Failed.\n");
     return EXIT_FAILURE;
-  if(power(0,0) !=1)
+  }
+  if(power(0,0) !=1){
+    printf("Failed.\n");
     return EXIT_FAILURE;
-  if(power(-2,1) !=(-2))
+  }
+  if(power(-2,1) !=(-2)){
+    printf("Failed.\n");
     return EXIT_FAILURE;
+  }
+  printf("Success\n");
   return EXIT_SUCCESS;
 }
