c968ce96e33903ae16ebd8de11689678cdbfa93c
Coursera Learner <nobody@nowhere.nul>
2020-05-04 09:24:16 +0000

    Did Assignment 20.

---
+++ 20_rot_matrix/rotate.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void rotate(char matrix[10][10]){
+  int a,b,c,d;
+  char temp;  
+  for(a=0,b=9;a<5;a++,b--){
+    for(c=a,d=b;c<b;c++,d--){
+      temp = matrix[a][c];
+      matrix[a][c] = matrix[d][a];
+      matrix[d][a] = matrix[b][d];
+      matrix[b][d] = matrix[a][b];
+      matrix[a][b] = temp;
+    }
+  }
+  
+}
