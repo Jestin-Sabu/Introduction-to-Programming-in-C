983d37dcec30db09ecea53ca0561b3b21a8484c4
Coursera Learner <nobody@nowhere.nul>
2020-05-12 04:16:20 +0000

    Did Assignment 25.

---
+++ 25_break_encr/breaker.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+
+int keyfinder(FILE* f){
+  int alpha[26] = {0},max,j,c;
+  while((c=fgetc(f)) != EOF){
+    if(isalpha(c)){
+      c -= 'a';
+      alpha[c]++;
+    }
+  }
+  max = alpha[0];
+  for(int i=0;i<26;i++){
+    if(alpha[i]>max){
+      max = alpha[i];
+      j=i;
+    }
+  }
+  return j-'a'-'e';
+}
+int main(int argc, char** argv){
+  if(argc != 2){
+    fprintf(stderr,"Usage: inputFilename\n");
+    return EXIT_FAILURE;
+  }
+  FILE* f = fopen(argv[1],"r");
+  if(f == NULL){
+    perror("Could not open the file\n");
+    return EXIT_FAILURE;
+  }
+  int key = keyfinder(f);
+  printf("%d\n",key);
+  fclose(f);
+  return EXIT_SUCCESS;
+}
+  
