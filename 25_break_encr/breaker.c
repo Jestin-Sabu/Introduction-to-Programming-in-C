8b50bad390349d96567bfe79d90ffc522858194d
Coursera Learner <nobody@nowhere.nul>
2020-05-12 06:00:15 +0000

    Made Corrections.

--- 25_break_encr/breaker.c
+++ 25_break_encr/breaker.c
@@ -1,44 +1,56 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-int keyfinder(FILE* f){
-  int alpha[26] = {0},max,j,c;
+int findkey(FILE *f){
+  int arr[26]={0};
+  int c;
+
   while((c=fgetc(f)) != EOF){
-    if(isalpha(c)){
-      c = tolower(c);
+    if (isalpha(c)) {
+      c=tolower(c);
       c -= 'a';
-      alpha[c]++;
+      arr[c] ++;
     }
   }
-  max = alpha[0];
-  for(int i=0;i<26;i++){
-    if(alpha[i]>max){
-      max = alpha[i];
-      j=i;
-    }
+  int max=0;
+  int j=0;
+  for (int i=0 ;i<26; i++){
+    if(arr[i] > max){
+      max=arr[i];
+      j=i;}
   }
-    if(j>=('e'-'a'))
-       return j-('e'-'a');
-    else
-      return (26-('e'-'a'))+j;
+  int k =  0;
+  if (j >= ('e' - 'a')) k=j-('e'-'a') ;
+  else k=(26-('e'-'a'))+ j;
+  return k ;
 }
-int main(int argc, char** argv){
-  if(argc != 2){
-    fprintf(stderr,"Usage: inputFilename\n");
+
+
+
+
+
+int main(int argc,char** argv){
+  if (argc != 2 ){
+    fprintf(stderr,"not enough arguments\n");
     return EXIT_FAILURE;
   }
-  FILE* f = fopen(argv[1],"r");
-  if(f == NULL){
-    perror("Could not open the file\n");
+
+  FILE * f= fopen(argv[1] ,"r");
+
+  if (f == NULL) {
+    perror("Could not open file");
     return EXIT_FAILURE;
   }
-  int key = keyfinder(f);
+
+  int key= findkey(f);
   printf("%d\n",key);
-  if(fclose(f)!=0){
-    perror("could not close the file\n");
+
+  if (fclose(f) != 0) {
+    perror("Failed to close the input file!");
     return EXIT_FAILURE;
   }
+
   return EXIT_SUCCESS;
+
 }
-  
