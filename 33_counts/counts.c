341ce7486a5cb7655c0b0d71c6050367f649b82b
Coursera Learner <nobody@nowhere.nul>
2020-05-16 07:20:13 +0000

    Did Assignment 33.

--- 33_counts/counts.c
+++ 33_counts/counts.c
@@ -3,15 +3,48 @@
 #include <string.h>
 #include "counts.h"
 counts_t * createCounts(void) {
-  //WRITE ME
+  counts_t *temp = malloc(sizeof(*temp));
+  temp->count = NULL;
+  temp->numcount = 0;
+  temp->unknown = 0;
+  return temp;
 }
 void addCount(counts_t * c, const char * name) {
-  //WRITE ME
+  if(name == NULL)
+    c->unknown++;
+  else{
+    int  flag =0;
+    int limit = c->numcount;
+    for(int i=0;i<limit;i++){
+      if((strcmp((c->count[i]->name), name)) == 0){
+	c->count[i]->count++;
+	flag = 1;
+      }
+    }
+    if(flag == 0){
+      one_count_t *temp = malloc(sizeof(*temp));
+      temp->name = malloc(sizeof(name));
+      strcpy(temp->name, name);
+      temp->count = 1;;
+      c->count = realloc((c->count), (c->numcount+1)*sizeof(*c->count));
+      c->count[c->numcount++] = temp;
+    }
+  }
 }
 void printCounts(counts_t * c, FILE * outFile) {
-  //WRITE ME
+  int limit = c->numcount;
+  for(int i=0;i<limit;i++)
+    fprintf(outFile,"%s: %d\n",c->count[i]->name,c->count[i]->count);
+  if(c->unknown != 0)
+    fprintf(outFile,"<unknown>: %d\n",c->unknown);
 }
 
 void freeCounts(counts_t * c) {
-  //WRITE ME
+  int limit = c->numcount;
+  for(int i=0;i<limit;i++){
+    free(c->count[i]->name);
+    free(c->count[i]);
+  }
+  free(c->count);
+  free(c);
 }
