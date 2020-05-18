2c26bbc98f7fe25d21ebd8ecb3582532fbb6cfe2
Coursera Learner <nobody@nowhere.nul>
2020-05-18 02:54:22 +0000

    Made Corrections.

--- 33_counts/counts.h
+++ 33_counts/counts.h
@@ -1,15 +1,18 @@
 #ifndef __COUNTS_H__
 #define __COUNTS_H__
 struct _one_count_t {
-  char *name;
-  int count;
+  //DEFINE ME
+  char* str;
+  size_t cnt;
 };
 typedef struct _one_count_t one_count_t;
 
 struct _counts_t {
-  one_count_t **count;
-  int numcount;
-  int unknown;
+  one_count_t** cntArr;
+  size_t cntSize;
+  size_t unkown;
+
+  //DEFINE ME
 };
 typedef struct _counts_t counts_t;
 
