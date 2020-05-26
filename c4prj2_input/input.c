d465ff0938aae9abd3693b7174f964a34f02326a
Coursera Learner <nobody@nowhere.nul>
2020-05-26 01:54:58 +0000

    Made Changes.

--- c4prj2_input/input.c
+++ c4prj2_input/input.c
@@ -1,15 +1,52 @@
-#ifndef FUTURE_H
-#define FUTURE_H
+
+#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 #include "deck.h"
+#include "cards.h"
+#include "future.h"
+deck_t * hand_from_string(const char * str, future_cards_t * fc){
+  deck_t* deck=malloc(sizeof(*deck));
+  deck->cards = NULL;
+  deck->n_cards=0;
+  for(int i=0;i<strlen(str);i++){
+    if((str[i] == '\n')||(str[i] == ' '))continue;
+    else{
+      if(str[i] == '?'){
+	i++;
+	char num[strlen(str)];
+	int n=0;
+	while(!((str[i] == '\n')||(str[i] == ' '))) {
+	  num[n]=str[i];
+	  i++;n++;}
+	num[n]='\0';
+	add_future_card(fc,atoi(num),add_empty_card(deck)) ;
 
-
-struct future_cards_tag {
-  deck_t * decks;
-  size_t n_decks;
-};
-typedef struct future_cards_tag future_cards_t;
-void add_future_card(future_cards_t * fc, size_t index, card_t * ptr) ;
-void future_cards_from_deck(deck_t * deck, future_cards_t * fc);
-
-
-#endif 
+      }
+      else{
+	card_t x = card_from_letters(str[i],str[i+1]);
+	add_card_to(deck,x);
+	i++;}
+    }
+  }
+  if (deck->n_cards < 5 ){
+    return NULL;
+    fprintf(stderr,"asas");}
+  return deck;
+}
+deck_t ** read_input(FILE * f, size_t * n_hands, future_cards_t * fc){
+  deck_t **arr=NULL;
+  size_t n_hand=0;
+  char *line=NULL;
+  size_t sz=0;
+  while(getline(&line,&sz,f)>=0){
+    arr=realloc(arr,(n_hand+1)*sizeof(*arr));
+    deck_t*deck=hand_from_string(line,fc);
+    if (deck == NULL)continue;
+    arr[n_hand]=deck;
+    n_hand ++;
+  }
+  *n_hands=n_hand;
+  return arr;
+}
